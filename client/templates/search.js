46bee4544898997679f9f072c4a89014b3a4e94b
Jelena <neohuilingjelena@gmail.com>
2017-06-06 20:48:39 +0800

    Fixed indentation for index.html, mapping.html and search.js

--- client/templates/search.js
+++ client/templates/search.js
@@ -1,29 +1,28 @@
 Template.search.events({
   'submit form': function (event) {
-    event.preventDefault();
+  	event.preventDefault();
 
-    // Get value from form element
-    // const target = event.target;
-    // const text = target.text.value;
-
-    Router.go('results');
-  },
+	// Get value from form element
+	// const target = event.target;
+	// const text = target.text.value;
+	Router.go('results');
+	}
 });
 /*
 
 an example of form submission
 
 Template.addList.events({
-    'submit form': function(event){
-        event.preventDefault();
-        var listName = $('[name=listName]').val();
-        Lists.insert({
-          name: listName
-        }, function(error, results){
-            Router.go('listPage', { _id: results });
-        });
-        $('[name=listName]').val('');
-    }
+	'submit form': function(event){
+		event.preventDefault();
+		var listName = $('[name=listName]').val();
+		Lists.insert({
+		  name: listName
+		}, function(error, results){
+			Router.go('listPage', { _id: results });
+		});
+		$('[name=listName]').val('');
+	}
 });
 
 */
