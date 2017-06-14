4fb2a41f7eca8c1eb8a8f883eaddead414e037d4
Jelena <neohuilingjelena@gmail.com>
2017-06-14 14:49:17 +0800

    Removed redundant 'module' function and corrected code style

--- client/templates/search.js
+++ client/templates/search.js
@@ -1,14 +1,14 @@
-Template.search.onCreated( () => {
+Template.search.onCreated(() => {
   let template = Template.instance();
 
   template.searchQuery = new ReactiveVar();
-  template.searching   = new ReactiveVar( false );
+  template.searching = new ReactiveVar(false);
 
-  template.autorun( () => {
-    template.subscribe( 'modules', template.searchQuery.get(), () => {
-      setTimeout( () => {
-        template.searching.set( false );
-      }, 300 );
+  template.autorun(() => {
+    template.subscribe('modules', template.searchQuery.get(), () => {
+      setTimeout(() => {
+        template.searching.set(false);
+      }, 300);
     });
   });
 });
@@ -22,28 +22,24 @@ Template.search.helpers({
   },
   modules() {
     let modules = Modules.find();
-    if ( modules ) {
+    if (modules) {
       return modules;
     }
+    return null;
   },
-  wasPrevMatch(isTrue) {
-    if ( isTrue ) {
-      return html_entity_decode('&#10004;');
-    }
-  }
 });
 
 Template.search.events({
-  'keyup [name="search"]' ( event, template ) {
+  'keyup [name="search"]'(event, template) {
     let value = event.target.value.trim();
 
-    if ( value !== '' && event.keyCode === 13 ) {
-      template.searchQuery.set( value );
-      template.searching.set( true );
+    if (value !== '' && event.keyCode === 13) {
+      template.searchQuery.set(value);
+      template.searching.set(true);
     }
 
-    if ( value === '' ) {
-      template.searchQuery.set( value );
+    if (value === '') {
+      template.searchQuery.set(value);
     }
-  }
+  },
 });
