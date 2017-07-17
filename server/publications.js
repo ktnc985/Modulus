c298ee9ebddd040a79a2beb510a7ef7421bfb7dd
Jelena <neohuilingjelena@gmail.com>
2017-07-18 01:45:34 +0800

    Add NUS module information to display when searched

--- server/publications.js
+++ server/publications.js
@@ -2,16 +2,20 @@ import { Meteor } from 'meteor/meteor';
 import { Match, check } from 'meteor/check';
 
 
-Meteor.publish('modules', function(limit, PUField, regionField, search) {
+Meteor.publish('modules', function(startUp, limit, PUField, regionField, search) {
   check(search, Match.OneOf(String, null, undefined));
   check(regionField, Match.OneOf(String, null, undefined));
   check(PUField, Match.OneOf(String, null, undefined));
 
-  let query = {};
-  const projection = {limit: limit, sort: {PrevMatch: -1, Similarity: -1, created: 1}};
+  if (startUp === true) {
+    return null;
+  }
+  
+  let query = {}, regex = new RegExp('');
+  const projection = {limit: 1, sort: {PrevMatch: -1, Similarity: -1, created: 1}};
   // sort in descending order according to Similarity field
   if (search) {
-    let regex = new RegExp(`^`+search+`$`, 'i'); //search for PU modules that are mapped exactly to the NUS module code entered by user
+    regex = new RegExp(`^`+search+`$`, 'i'); //search for PU modules that are mapped exactly to the NUS module code entered by user
 
     if (PUField) {
       query['UniversityName'] = PUField;
@@ -22,7 +26,9 @@ Meteor.publish('modules', function(limit, PUField, regionField, search) {
     }
 
     query['NUSModuleCode'] = regex;
+
+    projection.limit = limit;
   }
 
-  return Modules.find(query, projection);
+  return [NUSMods.find({'ModuleCode': regex}), Modules.find(query, projection)];
 });
