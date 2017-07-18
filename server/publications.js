7f9e25c87e260e712066daf98659016c406c5fae
Jelena <neohuilingjelena@gmail.com>
2017-07-18 13:51:07 +0800

    Add checks to parameters

--- server/publications.js
+++ server/publications.js
@@ -6,13 +6,15 @@ Meteor.publish('modules', function(startUp, limit, PUField, regionField, search)
   check(search, Match.OneOf(String, null, undefined));
   check(regionField, Match.OneOf(String, null, undefined));
   check(PUField, Match.OneOf(String, null, undefined));
+  check(limit, Match.OneOf(Number));
+  check(startUp, Match.OneOf(Boolean, undefined));
 
   if (startUp === true) {
     return null;
   }
   
   let query = {}, regex = new RegExp('');
-  const projection = {limit: 1, sort: {PrevMatch: -1, Similarity: -1, created: 1}};
+  const projection = {limit: limit, sort: {PrevMatch: -1, Similarity: -1, created: 1}};
   // sort in descending order according to Similarity field
   if (search) {
     regex = new RegExp(`^`+search+`$`, 'i'); //search for PU modules that are mapped exactly to the NUS module code entered by user
